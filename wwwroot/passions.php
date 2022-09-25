c8ef37cc3d79e74975a8bc38b82575d20226a467
Matt Day <mattsday@gmail.com>
2022-09-25 10:16:40 +0100

    Actually added passions this time

---
+++ wwwroot/passions.php
@@ -0,0 +1,34 @@
+<?php
+    include('site.php');
+    page_header('Passions', 'passions');
+?>
+<p>I have a few passions in life that I don't always get to fuel.</p>
+
+<h2>BBQ</h2>
+<p>I really enjoy BBQ. I love the science, the taste, the experimenting, and - of course - cooking!</p>
+
+<p>Whenever I get an unexpected bonus or windfall I will likely spend it here. I have several types of BBQ now, including:</p>
+<ul>
+<li>Kamado Joe - perfect for long smoking or if I want something very, very hot.</li>
+<li>Weber Gas - brilliant for quickly cooking something with minimal fuss.</li>
+<li>CADAC Safari - it folds up fairly small and works like a trooper - brilliant for camping!</li>
+</ul>
+
+<p>I have owned other BBQs and accessories, but these are the three I use throughout the year.</p>
+
+<p>For the past few years I've even done my Christmas Goose / Turkey on the BBQ - you should totally try it too!</p>
+
+<h2>Technology</h2>
+<p>My home is very automated. I have all kinds of gizmos and gadgets. I've even created some of my own that I might share here one day.</p>
+
+<p>Nothing brings me more joy than solving a problem if I can get my hands dirty to fix it. Writing code or building things is something I get lost in.</p>
+
+<h2>Sustainability</h2>
+<p>For a very long time I've struggled to live healthily and diet. I still do. Attaching this to sustainability though makes it easier for me.</p>
+
+<p>I am not perfect, and do stupid things. But in general I want to leave this planet in a better state than I joined it - so if I can do little things to help then I will.</p>
+
+<?php
+    page_footer();
+?>
+
